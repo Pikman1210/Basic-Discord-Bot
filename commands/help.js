31d4f54af968b0ca53d629cf2733363fe5aec40f
Pikman1210 <72507796+Pikman1210@users.noreply.github.com>
2021-01-05 09:43:45 -0600

    Delete help.js

--- commands/help.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	name: 'help',
-	description: 'The help command',
-	execute(message, args) {
-		message.channel.send('My commands are: `~args-info` `~avatar` `~help` `~invite` `~mention-test` `~ping` `~prune`');
-	},
-};
